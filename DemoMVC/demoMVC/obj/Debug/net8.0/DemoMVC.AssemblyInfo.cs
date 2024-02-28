b7d627c6b32469583beabaf38ca8dc7f810576bb
ninh khánh ly <ninhkhanhly196@gmail.com>
2024-02-28 10:39:01 +0700

    CreateProject

--- DemoMVC/demoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/demoMVC/obj/Debug/net8.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6602456f33dd978d80015aac58ab0869aa73f5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85400327d663c365c2798339f6c5f75f7c53985f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
