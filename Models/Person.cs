d40f7ce360b0e44a6d308adf03b94fee984d9109
ninhkhanhly <98010064+ninhkhanhly@users.noreply.github.com>
2024-02-28 10:35:09 +0700

    Delete Models directory

--- Models/Person.cs
+++
@@ -1,9 +0,0 @@
-namespace DemoMVC.Models
-{
-    public class Person
-    {
-        public string CCCD { get; set; }
-        public string FullName { get; set; }
-        public DateTime BirthDay { get; set; }
-    }
-}
